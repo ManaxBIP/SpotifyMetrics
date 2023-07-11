ca2411d11d24b877106f3275fc131417d81aceb8
Dalyll Reguia <dalyll.reguia@ynov.com>
2023-07-11 02:01:45 +0200

    initialisation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var filePath = path.resolve(__dirname, '../routes/html/index.html');
+  res.sendFile(filePath);
 });
 
 module.exports = router;
