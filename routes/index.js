6b588b0d0fe0ca70b3fde458fc2749b8b8767867
Pablo <pablopjl64@gmail.com>
2023-07-11 02:41:50 +0200

    [CREATE] Files For Login Page (HTML, CSS && JS) [ADD] Logo.png && LogoMetrics.png

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,18 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var filePath = path.resolve(__dirname, '../routes/html/index.html');
+  var filePath = path.resolve(__dirname, '../routes/html/login.html');
   res.sendFile(filePath);
+});
+
+router.get('/login.css', function(req, res, next) {
+  var cssPath = path.resolve(__dirname, '../routes/stylesheets/login.css');
+  res.sendFile(cssPath);
+});
+
+router.get('/login.js', function(req, res, next) {
+  var cssPath = path.resolve(__dirname, '../routes/js/login.js');
+  res.sendFile(cssPath);
 });
 
 module.exports = router;
